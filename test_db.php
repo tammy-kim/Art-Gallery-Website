e5be4b2238b17638b51621b7debacf0dcd1baaf1
Mine <minesher@students.cs.ubc.ca>
2022-08-04 09:46:40 -0700

    added comments to test_db

--- test_db.php
+++ test_db.php
@@ -3,7 +3,7 @@
 <html>
     
 <head>
-    <title>Art Owner Page</title>
+    <title>Testing db</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
@@ -20,18 +20,21 @@
         // test_db.php
         
         require('ac_db.inc.php');
-        
+        // this sets up a connection to the oracle database. The names don't really matter
         $db = new \Oracle\Db("test_db", "Mine");
-        $a = "CREATE TABLE demoTable (id int PRIMARY KEY, name char(30))";
+        // sql to make a demo table and insert one value
+        $a = "CREATE TABLE demoTable (id int PRIMARY KEY, name char(30)), INSERT INTO demoTable VALUES (1, 'meen')";
+        // actually runs the sql code above
         $db->execute($a, "create table");
-        $b = "INSERT INTO demoTable VALUES (1, 'meen')";
-        $db->execute($b, "insert me");
         $sql = "SELECT id, name FROM demoTable ORDER BY id";
+        // runs a sql query that returns values. the returned info is stored in res
         $res = $db->execFetchAll($sql, "Query Example");
         // echo "<pre>"; var_dump($res); echo "</pre>\n";
         
+        // create table
         echo "<table border='1'>\n";
         echo "<tr><th>id</th><th>name</th></tr>\n";
+        // adds each row of information in res to the table
         foreach ($res as $row) {
             $id = htmlspecialchars($row['ID'], ENT_NOQUOTES, 'UTF-8');
             $name   = htmlspecialchars($row['NAME'], ENT_NOQUOTES, 'UTF-8');
