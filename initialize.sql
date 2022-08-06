1105a88e362c239ecdba7cf33ca1261203c1dcac
UBC Student <tammykim@gambier.students.cs.ubc.ca>
2022-08-05 20:21:40 -0700

    Add comment

--- initialize.sql
+++ initialize.sql
@@ -12,3 +12,5 @@ insert into ArtOwner values(1, 'Tom', 'Nook', 'TomNook@islandmail.com');
 insert into ArtOwner values(2, 'Timmy', 'Nook', 'Timmy@islandmail.com');
 insert into ArtOwner values(3, 'Tommy', 'Nook', 'Tommy@islandmail.com');
 insert into ArtOwner values(4, 'Isabelle', NULL, 'Isabelle@islandmail.com');
+
+// comment
