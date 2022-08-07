d54e6e15da9177c7cac157abefb2bc34725d0c94
UBC Student <tammykim@students.cs.ubc.ca>
2022-08-07 15:43:22 -0700

    Update art_owner.php

--- art_owner.php
+++ art_owner.php
@@ -5,33 +5,33 @@
                  * Modified by Simona Radu
                  * Modified by Jessica Wong (2018-06-22)
                  */
-            
+
             $success = True; //keep track of errors so it redirects the page only if there are no errors
             $db_conn = NULL; // edit the login credentials in connectToDB()
             $show_debug_alert_messages = False; // set to True if you want alerts to show you which methods are being triggered (see how it is used in debugAlertMessage())
 
             function debugAlertMessage($message) {
                 global $show_debug_alert_messages;
-    
+
                 if ($show_debug_alert_messages) {
                     echo "<script type='text/javascript'>alert('" . $message . "');</script>";
                 }
             }
-            
+
             function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
                 //echo "<br>running ".$cmdstr."<br>";
                 global $db_conn, $success;
-    
+
                 $statement = OCIParse($db_conn, $cmdstr);
                 //There are a set of comments at the end of the file that describe some of the OCI specific functions and how they work
-    
+
                 if (!$statement) {
                     echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
                     $e = OCI_Error($db_conn); // For OCIParse errors pass the connection handle
                     echo htmlentities($e['message']);
                     $success = False;
                 }
-    
+
                 $r = OCIExecute($statement, OCI_DEFAULT);
                 if (!$r) {
                     echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
@@ -39,17 +39,17 @@
                     echo htmlentities($e['message']);
                     $success = False;
                 }
-    
+
                 return $statement;
             }
-            
+
             function connectToDB() {
                 global $db_conn;
-    
+
                 // Your username is ora_(CWL_ID) and the password is a(student number). For example,
                 // ora_platypus is the username and a12345678 is the password.
-                $db_conn = OCILogon("ora_minesher", "a28495142", "dbhost.students.cs.ubc.ca:1522/stu");
-    
+                $db_conn = OCILogon("ora_tammykim", "a57998726", "dbhost.students.cs.ubc.ca:1522/stu");
+
                 if ($db_conn) {
                     debugAlertMessage("Database is Connected");
                     return true;
@@ -63,7 +63,7 @@
 
             function disconnectFromDB() {
                 global $db_conn;
-    
+
                 debugAlertMessage("Disconnect from Database");
                 OCILogoff($db_conn);
             }
@@ -71,12 +71,19 @@
             function printArtOwnerResult($result) { //prints results from a select statement
                 echo "<br>List of registered Art Owners<br>";
                 echo "<table>";
-                echo "<tr><th>fn</th><th>ln</th></tr>";
-    
+                // echo "<tr><th>fn</th><th>ln</th></tr>";
+
+                // while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+                //     echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td></tr>"; //or just use "echo $row[0]"
+                // }
+
+                // I added email attribute because i think it's helpful info? Also makes it easier to debug stuff
+                echo "<tr><th>fn</th><th>ln</th><th>email</th></tr>";
+
                 while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-                    echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td></tr>"; //or just use "echo $row[0]"
+                    echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td></tr>"; //or just use "echo $row[0]"
                 }
-    
+
                 echo "</table>";
             }
 
@@ -84,25 +91,25 @@
                 echo "<br>Retrieved data from table demoTable:<br>";
                 echo "<table>";
                 echo "<tr><th>ID</th><th>Name</th></tr>";
-    
+
                 while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-                    echo "<tr><td>" . $row["ID"] . "</td><td>" . $row["NAME"] . "</td></tr>"; //or just use "echo $row[0]" 
+                    echo "<tr><td>" . $row["ID"] . "</td><td>" . $row["NAME"] . "</td></tr>"; //or just use "echo $row[0]"
                 }
-    
+
                 echo "</table>";
             }
 
             function handleDisplayRequest() {
                 global $db_conn;
-    
+
                 $result = executePlainSQL("SELECT id, name FROM demoTable");
-    
+
                 // if (($row = oci_fetch_row($result)) != false) {
                     printResult($result);
                 // }
             }
-            
-            
+
+
             function handleArtOwnerDisplayRequest(){
                 //$db = new \Oracle\Db("initialize", "Mine");
                 global $db_conn;
@@ -111,7 +118,7 @@
                 // runs a sql query that returns values. the returned info is stored in res
                 //$res = $db->execFetchAll($sql, "Query Example");
                 //$res = executePlainSQL($sql);
-                $res = executePlainSQL("SELECT FirstName, LastName FROM ArtOwner");
+                $res = executePlainSQL("SELECT FirstName, LastName, Email FROM ArtOwner");
                 OCICommit($db_conn);
                 // echo "<pre>"; var_dump($res); echo "</pre>\n";
                 // create table
@@ -134,17 +141,17 @@
             In this case you don't need to create the statement several times. Bound variables cause a statement to only be
             parsed once and you can reuse the statement. This is also very useful in protecting against SQL injection.
             See the sample code below for how this function is used */
-    
+
                 global $db_conn, $success;
                 $statement = OCIParse($db_conn, $cmdstr);
-    
+
                 if (!$statement) {
                     echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
                     $e = OCI_Error($db_conn);
                     echo htmlentities($e['message']);
                     $success = False;
                 }
-    
+
                 foreach ($list as $tuple) {
                     foreach ($tuple as $bind => $val) {
                         //echo $val;
@@ -152,7 +159,7 @@
                         OCIBindByName($statement, $bind, $val);
                         unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
                     }
-    
+
                     $r = OCIExecute($statement, OCI_DEFAULT);
                     if (!$r) {
                         echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
@@ -170,7 +177,7 @@
                 // // Drop old table
                 // $dropsql = "DROP TABLE ArtOwner";
                 // executePlainSQL($dropsql);
-    
+
                 // // Create new table
                 // echo "<br> resetting art owner table <br>";
                 // //$sql = file_get_contents('initialize.sql');
@@ -191,7 +198,7 @@
                 global $db_conn;
 
                 //Getting the values from user and insert data into the table
-                
+
                 $rowcount = executePlainSQL("SELECT Count(*) FROM ArtOwner");
                 $row = oci_fetch_row($rowcount);
 
@@ -206,16 +213,16 @@
                 // // $fn = $_POST['insFN'];
                 // // $ln = $_POST['insLN'];
                 // // $em = $_POST['insemail'];
-                
-                $ownerid = "$row[0]" + 1;
+
+                // $ownerid = "$row[0]" + 1;
+                $ownerid = $rowcount + 1;
                 //echo"$ownerid";
                 $alltuples = array (
                     $tuple
                 );
                 //$sql = "INSERT INTO ArtOwner values ($ownerid, '" . $fn . "', '" . $ln . "', '" . $em . "')";
                 // //$sql = "INSERT INTO ArtOwner values (5, 'abcf', 'deff', 'asdfasdff')";
-                
-                
+
                 executeBoundSQL("insert into ArtOwner values ($ownerid, :bind1, :bind2, :bind3)", $alltuples);
                 OCICommit($db_conn);
             }
@@ -248,7 +255,7 @@
 
 
                 disconnectFromDB();
-            
+
             }
 
             function handleGETRequest() {
@@ -259,14 +266,14 @@
                     } else if (array_key_exists('displayTuples', $_GET)) {
                         handleDisplayRequest();
                     }
-    
-                    
+
+
                 }
                 disconnectFromDB();
             }
 
             if (isset($_POST['reset']) || isset($_POST['insertSubmit'])|| isset($_POST['insertNewValue'])|| isset($_POST['deleteOwner'])) {
-                
+
                 handlePOSTRequest();
             } else if (isset($_GET['display']) ) {
                 //echo"1";
